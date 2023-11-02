7503fc85d261136e4e2ef80730e8d19a1b7db287
DevaMar1a <146069739+DevaMar1a@users.noreply.github.com>
2023-11-02 18:28:41 +0400

    lab 3

--- lab 3/3-2.sql
+++ lab 3/3-2.sql
@@ -3,3 +3,4 @@ USE cd;
 SELECT facilities.facility FROM facilities
 INNER JOIN bookings ON facilities.facid = bookings.facid
 WHERE facilities.facility LIKE '%Tennis Court%' AND DATE(bookings.starttime) = '2012-09-19';
+

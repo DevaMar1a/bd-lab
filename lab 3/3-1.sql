a5ded70422d4797ea9da941d3bfe5765d7a6cffc
DevaMar1a <146069739+DevaMar1a@users.noreply.github.com>
2023-11-07 18:20:28 +0400

    lab 3

--- lab 3/3-1.sql
+++ lab 3/3-1.sql
@@ -1,4 +1,4 @@
-/*Выберите лист в рамке (timestamp) покупки времени объектов членом клуба'David Farrell'*/
+/*Выберите лист в рамке (timestamp) покупки времени объектов членом клуба'David Farrell' */
 USE cd;
 SELECT starttime FROM bookings
 JOIN members ON bookings.memid = members.memid

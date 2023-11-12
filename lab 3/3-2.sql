3d250bd8b57614996a2a7bd9915bc0ca367e2125
DevaMar1a <146069739+DevaMar1a@users.noreply.github.com>
2023-11-12 16:30:12 +0400

    lab 3

--- lab 3/3-2.sql
+++ lab 3/3-2.sql
@@ -1,6 +1,5 @@
 /* Выберите теннисные корты, заброниррованные пользователями на 19 сентября 2012 года */
 USE cd;
-SELECT facilities.facility FROM facilities
-INNER JOIN bookings ON facilities.facid = bookings.facid
-WHERE facilities.facility LIKE '%Tennis Court%' AND DATE(bookings.starttime) = '2012-09-19';
-
+SELECT facility FROM facilities
+JOIN bookings ON facilities.facid = bookings.facid
+WHERE facilities.facility LIKE '%Tennis court%' AND DATE(bookings.starttime)="2012-09-19";

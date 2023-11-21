92214ec15bbc32eb4f0dca4bf58906efd00c8cd6
DevaMar1a <146069739+DevaMar1a@users.noreply.github.com>
2023-11-21 13:41:47 +0400

    lab 5

--- lab 5/5-3.sql
+++ lab 5/5-3.sql
@@ -2,11 +2,5 @@
 данных этим членом клуба, искючив членов, которые не давали 
 рекомендаций.*/
 USE cd;
-SELECT m.memid AS MemberID,
-m.firstname AS firstname,
-m.surname AS lastname,
-COUNT(m2.memid) AS r_count
-FROM members m
-JOIN members m2 ON m.memid = m2.recommendedby
-WHERE m.recommendedby IS NOT NULL
-GROUP BY m.memid, m.firstname, m.surname;
+SELECT CONCAT(m1.surname, ' ', m1.firstname) AS 'ФИО', COUNT(m2.memid) AS 'Количество рекомендаций' FROM members m1
+JOIN members AS m2 ON m2.recommendedby = m1.memid GROUP BY m1.memid;

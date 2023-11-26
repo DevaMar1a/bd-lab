6792572bf400e9b86915f4be9cb940665e381181
DevaMar1a <146069739+DevaMar1a@users.noreply.github.com>
2023-11-26 20:28:14 +0400

    lab 5

--- lab 5/5-17.sql
+++ lab 5/5-17.sql
@@ -2,16 +2,21 @@
 Упорядочите по классификации и названию объекта.
 Примечание: используйте функцию деления на группы ntile.*/
 USE cd;
-SELECT facility, 
-CASE 
-  WHEN NTILE(3) OVER (ORDER BY revenue) = 1 THEN 'Высокий доход'
-  WHEN NTILE(3) OVER (ORDER BY revenue) = 2 THEN 'Средний доход'
-  WHEN NTILE(3) OVER (ORDER BY revenue) = 3 THEN 'Низкий доход'
-END AS incomegroup
-FROM (
-  SELECT f.facility, SUM(IF(b.memid = 0, f.guestcost * b.slots, f.membercost * b.slots)) AS revenue
-  FROM facilities f
-  INNER JOIN bookings b ON f.facid = b.facid
-  GROUP BY f.facility
-) AS subquery
-ORDER BY incomegroup, facility;
+WITH IncomeGroups AS (
+    SELECT 
+        fac.facility,
+        SUM(book.slots) AS Total_Income,
+        NTILE(3) OVER (ORDER BY SUM(book.slots)) AS Income_Group
+    FROM bookings AS book
+    INNER JOIN facilities AS fac ON book.facid = fac.facid
+    GROUP BY fac.facility
+)
+SELECT 
+    facility AS 'Название объекта',
+    CASE 
+        WHEN Income_Group = 1 THEN 'высокий'
+        WHEN Income_Group = 2 THEN 'средний'
+        ELSE 'низкий' 
+    END AS Income_Category
+FROM IncomeGroups
+ORDER BY revenue_group, facility;

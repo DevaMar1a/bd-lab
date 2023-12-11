a16cbff6f1b8a21858931e4ffd176ea0a6429fb1
DevaMar1a <146069739+DevaMar1a@users.noreply.github.com>
2023-12-11 09:54:03 +0400

    laba 6

--- lab 6/6-6.sql
+++ lab 6/6-6.sql
@@ -1,5 +1,4 @@
 /*Выведите все объектов клуба, если они содержат в названии слово  ‘Tennis’ в произвольном регистре.*/
 USE cd;
-SELECT f.facility
-  FROM facilities f
-  WHERE f.facility LIKE '%Tennis%'
+USE cd;
+SELECT facility FROM facilities WHERE facility like '%Tennis%';

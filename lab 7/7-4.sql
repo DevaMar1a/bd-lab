f4d66a2b98d8beffb458160c5d6158a4fb90264b
DevaMar1a <146069739+DevaMar1a@users.noreply.github.com>
2023-12-26 16:01:13 +0400

    lab 7

--- lab 7/7-4.sql
+++ lab 7/7-4.sql
@@ -3,37 +3,58 @@
 (в процентах) на основании расчета окупаемости за уже оплаченные периоды. Сохраните расчет в виде csv 
 или sql файла (например, используя временные таблицы).*/
 USE cd;
-DELIMITER //
-DROP FUNCTION IF EXISTS CalculateRentChange // 
-CREATE FUNCTION CalculateRentChange(in_percentage_change DECIMAL(5,2))
-RETURNS TEXT
-DETERMINISTIC
-BEGIN
-    -- Создание временной таблицы для расчета
-    CREATE TEMPORARY TABLE IF NOT EXISTS RentChange AS
-    SELECT 
-        bookings.facid,
-        SUM(CASE 
-                WHEN bookings.memid = 0 THEN facilities.guestcost * bookings.slots
-                ELSE facilities.membercost * bookings.slots
-            END) AS rent,
-        SUM(payments.payment) - facilities.monthlymaintenance AS profit
-    FROM bookings
-    LEFT JOIN facilities ON bookings.facid = facilities.facid
-    LEFT JOIN payments ON bookings.bookid = payments.bookid
-    GROUP BY bookings.facid;
 
-    -- Расчет изменения стоимости аренды
-    UPDATE RentChange 
-    SET rent = 
-        CASE 
-            WHEN (profit <= 0 OR rent <= 0) THEN 0
-            ELSE rent * (1 + in_percentage_change / 100)
-        END;
-    -- Возвращение сообщения об успешном завершении
-    RETURN 'Изменения аренды рассчитаны и сохранены в sql файл';
-END//
+DELIMITER //
+
+DROP FUNCTION IF EXISTS increase_income_by //
+CREATE FUNCTION increase_income_by(facid INT, fraction FLOAT, curtime TIMESTAMP)
+  RETURNS VARCHAR(50)
+  READS SQL DATA
+  NOT DETERMINISTIC
+  BEGIN
+    DECLARE income DECIMAL(10, 2);
+    DECLARE maintenance DECIMAL(10, 2);
+    DECLARE net_income DECIMAL(10, 2);
+    DECLARE cost DECIMAL(10, 2);
+    DECLARE beta DECIMAL(10, 2);
+   
+    SELECT SUM(p.payment) INTO income
+      FROM payments AS p
+        JOIN bookings AS b ON b.bookid = p.bookid
+        JOIN facilities AS f ON b.facid = f.facid
+      WHERE facid = b.facid AND
+        b.starttime <= curtime
+      GROUP BY b.facid;
+
+    IF income IS NULL THEN RETURN 1;
+    END IF;
+
+    SELECT f.monthlymaintenance * (MONTH(curtime) - MONTH(MIN(b.starttime)) + 1) INTO maintenance
+      FROM facilities AS f
+        JOIN bookings AS b ON b.facid = f.facid
+      WHERE facid = b.facid
+      GROUP BY b.facid;
+
+    SELECT f.initialoutlay INTO cost
+      FROM facilities AS f
+      WHERE facid = f.facid;
+
+    SET net_income = (income - maintenance);
+
+    IF net_income <= 0 OR cost <= net_income THEN RETURN 1;
+    END IF;
+
+    SET beta = ((1 / fraction) * (cost - net_income)
+                / cost * (1 - maintenance / income) + maintenance / income);
+    
+    RETURN beta;
+  END //
 
 DELIMITER ;
-SELECT CalculateRentChange(10.54);
-SELECT * FROM RentChange;
+
+SELECT increase_income_by(2, 2, '2012-07-31-23:59:59')
+INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/output.csv' 
+FIELDS ENCLOSED BY '"' 
+TERMINATED BY ';' 
+ESCAPED BY '"' 
+LINES TERMINATED BY '\r\n';
